63b540f7ac61ac553529a4806942dd8ecd500877
SANTIAGO SUAREZ <santi_sz01@hotmail.com>
2023-09-28 16:55:07 -0300

    Agrega carpeta de clases + clase database

---
+++ clases/database.php
@@ -0,0 +1,30 @@
+<?php
+
+class Database{
+
+    private $hostname = "localhost";
+    private $database = "productos";
+    private $username = "root";
+    private $password = "";
+    
+    function conectar(){
+
+        try{
+        $conexion = "mysql:host=" . $this->hostname . 
+        "; dbname=" . $this->database; 
+        
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ];
+
+        $pdo = new PDO($conexion, $this->username, $this->password, $options);
+        
+        return $pdo;
+    } catch(PDOException $e){
+        echo 'Error conexion: ' . $e->getMessage();
+        exit;
+    }
+}
+}
+?>
